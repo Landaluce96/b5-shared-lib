7d16fee6166be173069735ee5e07c2726d8998bc
Landaluce <landaluce1996@gmail.com>
2023-05-04 17:27:26 +0200

    updated jacoco.groovy

--- vars/jacoco.groovy
+++ vars/jacoco.groovy
@@ -3,9 +3,4 @@ def jacocoTest(){
     withMaven(maven: 'jenkins-maven'){
         sh 'mvn jacoco:prepare-agent jacoco:report'
     }
-    jacoco(
-        execPattern: '**/build/jacoco/*.exec',
-        classPattern: '**/build/classes/java/main',
-        sourcePattern: '**/src/main'
-    )
 }
