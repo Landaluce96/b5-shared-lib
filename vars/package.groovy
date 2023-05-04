5d45463154998d8fc5ce8c36a13bef53e928c77c
Álvaro Landaluce Torralbo <102660969+Landaluce96@users.noreply.github.com>
2023-05-04 18:11:32 +0200

    Update package.groovy

--- vars/package.groovy
+++ vars/package.groovy
@@ -1,6 +1,16 @@
 
 def package(){
     stage('Package'){
-        sh 'mvn -B -DskipTests package dependency:copyDependencies'
+        try{
+            sh 'mvn -B -DskipTests package dependency:copyDependencies'
+        }catch{
+
+        }finally{
+            def currentResult = currentBuild.result
+            if (currentResult == 'SUCCESS') {
+                archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+                archiveArtifacts artifacts: 'target/dependency', fingerprint: true
+            }	
+        }
     }
 }
