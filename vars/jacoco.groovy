57907cea684f27cd5d4ea388ddc5c3997a7ea1c9
Landaluce <landaluce1996@gmail.com>
2023-05-05 09:49:05 +0200

    updated packageStage.groovy and jacoco.groovy

--- vars/jacoco.groovy
+++ vars/jacoco.groovy
@@ -1,4 +1,5 @@
 
 def jacocoTest(){
     sh 'mvn jacoco:prepare-agent jacoco:report'
+    archiveArtifacts artifacts: 'target/site/jacoco/', fingerprint: true
 }
--- vars/packageStage.groovy
+++ vars/packageStage.groovy
@@ -1,22 +1,7 @@
 
 def packageStage(){
-    try{
-        stage('Package'){
-            sh 'mvn -B -DskipTests package dependency:copy-dependencies'
-            echo "***************"
-            echo "*** ARCHIVE ***"
-            echo "***************"
-            archiveArtifacts artifacts: 'target/**/*.jar', fingerprint: true
-            echo "***************"
-            echo "*** ARCHIVE ***"
-            echo "***************"
-        }
-    }catch(err){
-
-    }finally{
-        def currentResult = currentBuild.result
-        if (currentResult == 'SUCCESS') {
-            
-        }	
+    stage('Package'){
+        sh 'mvn -B -DskipTests package dependency:copy-dependencies'
+        archiveArtifacts artifacts: 'target/**/*.jar', fingerprint: true
     }
 }
