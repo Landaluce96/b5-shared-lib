fc1075d25496282f8f4a0f2978c3000d232fccff
Landaluce <landaluce1996@gmail.com>
2023-05-05 09:31:38 +0200

    updated packageStage.groovy

--- vars/packageStage.groovy
+++ vars/packageStage.groovy
@@ -9,9 +9,15 @@ def packageStage(){
     }finally{
         def currentResult = currentBuild.result
         if (currentResult == 'SUCCESS') {
+            echo "***************"
+            echo "*** ARCHIVE ***"
+            echo "***************"
             zip zipfile: 'app.zip', archive: true, overwrite: true, glob: 'target/*.jar,target/dependency'
             archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
             archiveArtifacts artifacts: 'target/dependency/*.jar', fingerprint: true
+            echo "***************"
+            echo "*** ARCHIVE ***"
+            echo "***************"
         }	
     }
 }
