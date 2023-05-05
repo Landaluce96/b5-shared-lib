e03c3db494321e3ded4659c2ce31fd770215468f
Landaluce <landaluce1996@gmail.com>
2023-05-05 14:55:57 +0200

    updated deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -3,9 +3,9 @@ def deploy(){
     stage('Deploy'){
         sh 'mkdir docker'
         def dockerfile = libraryResource 'com/landaluce/Dockerfile'
-        writeFile file: 'docker/Dockerfile' text: dockerfile
+        writeFile file: 'docker/Dockerfile' text: "${dockerfile}"
         def dockerCompose = libraryResource 'com/landaluce/docker-compose.yml'
-        writeFile file: 'docker/docker-compose.yml' text: dockerCompose
+        writeFile file: 'docker/docker-compose.yml' text: "${dockerCompose}"
 
         sh 'docker compose -f docker/docker-compose.yml build'
     }
