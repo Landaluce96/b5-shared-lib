598f58d697bdd5ed96010b19ebfc28324e191e78
Landaluce <landaluce1996@gmail.com>
2023-05-05 09:00:43 +0200

    updated packageStage.groovy

--- vars/packageStage.groovy
+++ vars/packageStage.groovy
@@ -1,16 +1,16 @@
 
 def packageStage(){
-    stage('Package'){
-        try{
+    try{
+        stage('Package'){
             sh 'mvn -B -DskipTests package dependency:copyDependencies'
-        }catch(err){
-
-        }finally{
-            def currentResult = currentBuild.result
-            if (currentResult == 'SUCCESS') {
-                archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
-                archiveArtifacts artifacts: 'target/dependency', fingerprint: true
-            }	
         }
+    }catch(err){
+
+    }finally{
+        def currentResult = currentBuild.result
+        if (currentResult == 'SUCCESS') {
+            archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+            archiveArtifacts artifacts: 'target/dependency', fingerprint: true
+        }	
     }
 }
