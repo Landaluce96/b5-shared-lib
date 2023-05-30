772774b456570626a0c4746533b71ff844e212be
Álvaro Landaluce Torralbo <102660969+Landaluce96@users.noreply.github.com>
2023-05-30 09:18:14 +0200

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,11 +1,14 @@
 
 def deploy(){
     stage('Deploy'){
+        
         def dockerfile = libraryResource ('com/landaluce/Dockerfile')
         writeFile (file: 'Dockerfile', text: dockerfile)
         def dockerCompose = libraryResource ('com/landaluce/docker-compose.yml')
         writeFile (file: 'docker-compose.yml', text: dockerCompose)
-
-        sh 'docker compose build'
+        
+        withDocker(docker: 'Docker'){
+            sh 'docker compose build'
+        }        
     }
 }
