f6882475546c124944cb50108647579aa6dd722d
Álvaro Landaluce Torralbo <102660969+Landaluce96@users.noreply.github.com>
2023-05-30 10:04:22 +0200

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -9,7 +9,7 @@ def deploy(){
         
         docker.withTool('Docker'){
             String name = 'landaluce/b5-jenkins-app'
-            sh "docker build -t ${name}:${BUILD_ID} -t ${name}:latest"
+            sh "docker build -t ${name}:${BUILD_ID} -t ${name}:latest ."
             withCredentials([usernamePassword(credentialsId: 'Docker_hub', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
                 sh "docker login -u ${USER} -p ${PASSWORD}"
                 sh "docker push ${name}:${BUILD_ID}"
