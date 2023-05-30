56bed8f41bb54cd25b4854108ee3d1b2499c32b2
Álvaro Landaluce Torralbo <102660969+Landaluce96@users.noreply.github.com>
2023-05-30 10:44:04 +0200

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,16 +1,16 @@
 
 def deploy(){
     stage('Deploy'){
-        
-        withDockerRegistry(credentialsId: 'Docker_hub') {
+        docker.withTool('Docker'){
+            withDockerRegistry(credentialsId: 'Docker_hub') {
             
-            def dockerfile = libraryResource ('com/landaluce/Dockerfile')
-            writeFile (file: 'Dockerfile', text: dockerfile)
-            
-            def app = docker.build("landaluce/b5-jenkins-app:${BUILD_ID}")
-            app.push()
-            app.push 'latest'
+                def dockerfile = libraryResource ('com/landaluce/Dockerfile')
+                writeFile (file: 'Dockerfile', text: dockerfile)
+                def app = docker.build("landaluce/b5-jenkins-app:${BUILD_ID}")
+                app.push()
+                app.push 'latest'
+                
+            }
         }
-        
     }
 }
