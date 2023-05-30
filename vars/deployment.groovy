be0b4f7a67083c9f1efdb835671130933b1be18a
Álvaro Landaluce Torralbo <102660969+Landaluce96@users.noreply.github.com>
2023-05-30 10:02:37 +0200

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -8,7 +8,13 @@ def deploy(){
         writeFile (file: 'docker-compose.yml', text: dockerCompose)
         
         docker.withTool('Docker'){
-            sh 'docker compose build'
+            String name = 'landaluce/b5-jenkins-app'
+            sh "docker build -t ${name}:${BUILD_ID} -t ${name}:latest"
+            withCredentials([usernamePassword(credentialsId: 'Docker_hub', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
+                sh "docker login -u ${USER} -p ${PASSWORD}"
+                sh "docker push ${name}:${BUILD_ID}"
+                sh "docker push ${name}:latest"
+            }
         }        
     }
 }
